3210123e9ec17242df159c33c8c7f3081e603a0a
Elie Schoppik <eschoppik@gmail.com>
2018-04-06 13:41:45 -0400

    adding remove word

--- tries/trie.js
+++ tries/trie.js
@@ -7,8 +7,8 @@ class Trie {
     if (index === word.length) {
       this.isWord = true;
     } else if (index < word.length) {
-      let char = word[index];
-      let subTrie = this.characters[char] || new Trie();
+      var char = word[index];
+      var subTrie = this.characters[char] || new Trie();
       subTrie.addWord(word, index + 1);
       this.characters[char] = subTrie;
     }
@@ -16,7 +16,12 @@ class Trie {
   }
 
   findWord(word, index = 0) {
-    let char = word[index];
+    // This function will return the node in the trie
+    // which corresponds to the end of the passed in word.
+
+    // Be sure to consider what happens if the word is not in this Trie.
+
+    var char = word[index];
     if (index < word.length - 1 && this.characters[char]) {
       index += 1;
       return this.characters[char].findWord(word, index);
@@ -25,21 +30,56 @@ class Trie {
     }
   }
   getWords(words = [], currentWord = "") {
+    // This function will return all the words which are
+    // contained in this Trie.
+    // it will use currentWord as a prefix,
+    // since a Trie doesn't know about its parents.
+
     if (this.isWord) {
       words.push(currentWord);
     }
-    for (let char in this.characters) {
-      let nextWord = currentWord + char;
+    for (var char in this.characters) {
+      var nextWord = currentWord + char;
       this.characters[char].getWords(words, nextWord);
     }
     return words;
   }
   autoComplete(prefix) {
-    let subTrie = this.find(prefix);
+    // This function will return all completions
+    // for a given prefix.
+    // It should use find and getWords.
+    var subTrie = this.find(prefix);
     if (subTrie) {
       return subTrie.getWords([], prefix);
     } else {
       return [];
+    }
+  }
+  removeWord(word) {
+    let foundWord = this.findWord(word);
+    if (foundWord === undefined) {
+      return;
+    }
+
+    // if the word has additional characters, just remove the isWord property
+    if (Object.keys(foundWord.characters).length > 0) {
+      foundWord.isWord = false;
+      return;
+    }
+
+    // otherwise we have to make our way to that word and delete anytime there is only 1 characters property
+    let currentTrie = this;
+    let index = 0;
+    var char = word[index];
+    while (index < word.length - 1 && currentTrie) {
+      var char = word[index];
+      currentTrie = currentTrie.characters[char];
+      var skip = currentTrie.characters[word[index + 1]].characters;
+      if (Object.keys(skip).length === 1) {
+        delete currentTrie.characters[word[index + 1]];
+        break;
+      }
+      index++;
     }
   }
 }
