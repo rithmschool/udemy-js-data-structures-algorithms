a5b51f4b86ce0ab86e5cc15578489a463237f5c7
Matt Lane <lane.matthew@gmail.com>
2018-06-06 16:36:28 -0700

    radix tweaks

--- sorting/radix.js
+++ sorting/radix.js
@@ -1,5 +1,5 @@
 function getDigit(num, i) {
-  return Math.trunc(num / 10 ** i) % 10;
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
 function digitCount(num) {
@@ -9,7 +9,7 @@ function digitCount(num) {
 
 function mostDigits(nums) {
   let maxDigits = 0;
-  for (let i = 0; i < nums.length; nums++) {
+  for (let i = 0; i < nums.length; i++) {
     maxDigits = Math.max(maxDigits, digitCount(nums[i]));
   }
   return maxDigits;
